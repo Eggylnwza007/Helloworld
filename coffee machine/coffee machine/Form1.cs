d6b90fc65cd7de011566204a755d4da33d042620
Eggylnwza007 <kamom.j@kkumail.com>
2022-01-13 12:25:14 +0700

    up3 coffee machine
    coffee machine

---
+++ coffee machine/coffee machine/Form1.cs
@@ -0,0 +1,19 @@
+class coffee_machine{
+    int black_coffee;
+    int mocha;
+    int latte;
+    int chocolate;
+    int milk;
+    int water;
+    int coffee;
+    void coffee_machine(int black_coffee, int mocha, int latte, int chocolate) { }
+    void buyblack_coffee(int amont) { }
+    void buymocha(int amont) { }
+    void buylatte(int amont) { }
+    void buychocolate(int amont) { }
+    void ingredient() { }
+    void add_ingredient(int water, int coffee, int chocolate) { }
+}
+
+
+
