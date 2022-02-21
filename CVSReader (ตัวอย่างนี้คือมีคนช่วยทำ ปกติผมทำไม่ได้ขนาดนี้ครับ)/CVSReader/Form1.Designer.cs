97e1d514b50424c7a411aafeef2419d22373534a
Eggylnwza007 <96216608+Eggylnwza007@users.noreply.github.com>
2022-02-21 15:31:13 +0700

    Add files via upload

---
+++ CVSReader (ตัวอย่างนี้คือมีคนช่วยทำ ปกติผมทำไม่ได้ขนาดนี้ครับ)/CVSReader/Form1.Designer.cs
@@ -0,0 +1,381 @@
+﻿namespace CVSReader
+{
+    using System.Windows.Forms;
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.DGV = new System.Windows.Forms.DataGridView();
+            this.ColumnID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnMajor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TBID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BTSave = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TBName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.CBMajor = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.TBMin = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.TBMax = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.TBGpax = new System.Windows.Forms.TextBox();
+            this.TBGPA = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 1, 0, 1);
+            this.menuStrip1.Size = new System.Drawing.Size(780, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 22);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // DGV
+            // 
+            this.DGV.AllowUserToAddRows = false;
+            this.DGV.AllowUserToDeleteRows = false;
+            this.DGV.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.DGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGV.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnID,
+            this.ColumnName,
+            this.ColumnMajor,
+            this.Column1});
+            this.DGV.Location = new System.Drawing.Point(221, 20);
+            this.DGV.Margin = new System.Windows.Forms.Padding(2);
+            this.DGV.Name = "DGV";
+            this.DGV.ReadOnly = true;
+            this.DGV.RowHeadersWidth = 51;
+            this.DGV.RowTemplate.Height = 29;
+            this.DGV.Size = new System.Drawing.Size(538, 281);
+            this.DGV.TabIndex = 2;
+            this.DGV.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // ColumnID
+            // 
+            this.ColumnID.HeaderText = "ID";
+            this.ColumnID.MinimumWidth = 6;
+            this.ColumnID.Name = "ColumnID";
+            this.ColumnID.ReadOnly = true;
+            this.ColumnID.Width = 125;
+            // 
+            // ColumnName
+            // 
+            this.ColumnName.HeaderText = "Name";
+            this.ColumnName.MinimumWidth = 6;
+            this.ColumnName.Name = "ColumnName";
+            this.ColumnName.ReadOnly = true;
+            this.ColumnName.Width = 125;
+            // 
+            // ColumnMajor
+            // 
+            this.ColumnMajor.HeaderText = "Major";
+            this.ColumnMajor.MinimumWidth = 6;
+            this.ColumnMajor.Name = "ColumnMajor";
+            this.ColumnMajor.ReadOnly = true;
+            this.ColumnMajor.Width = 125;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "GPA";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // TBID
+            // 
+            this.TBID.Location = new System.Drawing.Point(79, 45);
+            this.TBID.Margin = new System.Windows.Forms.Padding(2);
+            this.TBID.Multiline = true;
+            this.TBID.Name = "TBID";
+            this.TBID.Size = new System.Drawing.Size(113, 24);
+            this.TBID.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 48);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "id";
+            // 
+            // BTSave
+            // 
+            this.BTSave.Location = new System.Drawing.Point(77, 149);
+            this.BTSave.Margin = new System.Windows.Forms.Padding(2);
+            this.BTSave.Name = "BTSave";
+            this.BTSave.Size = new System.Drawing.Size(113, 19);
+            this.BTSave.TabIndex = 5;
+            this.BTSave.Text = "save";
+            this.BTSave.UseVisualStyleBackColor = true;
+            this.BTSave.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 74);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "name";
+            // 
+            // TBName
+            // 
+            this.TBName.Location = new System.Drawing.Point(79, 72);
+            this.TBName.Margin = new System.Windows.Forms.Padding(2);
+            this.TBName.Multiline = true;
+            this.TBName.Name = "TBName";
+            this.TBName.Size = new System.Drawing.Size(113, 24);
+            this.TBName.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 101);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "major";
+            // 
+            // CBMajor
+            // 
+            this.CBMajor.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CBMajor.FormattingEnabled = true;
+            this.CBMajor.Items.AddRange(new object[] {
+            "CIS",
+            "DS"});
+            this.CBMajor.Location = new System.Drawing.Point(77, 98);
+            this.CBMajor.Margin = new System.Windows.Forms.Padding(2);
+            this.CBMajor.Name = "CBMajor";
+            this.CBMajor.Size = new System.Drawing.Size(114, 21);
+            this.CBMajor.TabIndex = 10;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.TBMin);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.TBMax);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.TBGpax);
+            this.groupBox1.Location = new System.Drawing.Point(29, 190);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(188, 95);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "ข้อมูลรวม";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 73);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(24, 13);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Min";
+            // 
+            // TBMin
+            // 
+            this.TBMin.Enabled = false;
+            this.TBMin.Location = new System.Drawing.Point(55, 70);
+            this.TBMin.Margin = new System.Windows.Forms.Padding(2);
+            this.TBMin.Multiline = true;
+            this.TBMin.Name = "TBMin";
+            this.TBMin.Size = new System.Drawing.Size(113, 24);
+            this.TBMin.TabIndex = 17;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 46);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(27, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Max";
+            // 
+            // TBMax
+            // 
+            this.TBMax.Enabled = false;
+            this.TBMax.Location = new System.Drawing.Point(55, 44);
+            this.TBMax.Margin = new System.Windows.Forms.Padding(2);
+            this.TBMax.Multiline = true;
+            this.TBMax.Name = "TBMax";
+            this.TBMax.Size = new System.Drawing.Size(113, 24);
+            this.TBMax.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 19);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "GPAx";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // TBGpax
+            // 
+            this.TBGpax.Enabled = false;
+            this.TBGpax.Location = new System.Drawing.Point(55, 17);
+            this.TBGpax.Margin = new System.Windows.Forms.Padding(2);
+            this.TBGpax.Multiline = true;
+            this.TBGpax.Name = "TBGpax";
+            this.TBGpax.Size = new System.Drawing.Size(113, 24);
+            this.TBGpax.TabIndex = 13;
+            // 
+            // TBGPA
+            // 
+            this.TBGPA.Location = new System.Drawing.Point(79, 122);
+            this.TBGPA.Margin = new System.Windows.Forms.Padding(2);
+            this.TBGPA.Multiline = true;
+            this.TBGPA.Name = "TBGPA";
+            this.TBGPA.Size = new System.Drawing.Size(113, 24);
+            this.TBGPA.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(37, 125);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "GPA";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(780, 309);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TBGPA);
+            this.Controls.Add(this.CBMajor);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TBName);
+            this.Controls.Add(this.BTSave);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TBID);
+            this.Controls.Add(this.DGV);
+            this.Controls.Add(this.menuStrip1);
+            this.KeyPreview = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem openToolStripMenuItem;
+        private ToolStripMenuItem saveToolStripMenuItem;
+        private DataGridView DGV;
+        private TextBox TBID;
+        private Label label1;
+        private Button BTSave;
+        private Label label2;
+        private TextBox TBName;
+        private Label label3;
+        private ComboBox CBMajor;
+        private GroupBox groupBox1;
+        private Label label7;
+        private TextBox TBMin;
+        private Label label6;
+        private TextBox TBMax;
+        private Label label5;
+        private TextBox TBGpax;
+        private TextBox TBGPA;
+        private Label label4;
+        private DataGridViewTextBoxColumn ColumnID;
+        private DataGridViewTextBoxColumn ColumnName;
+        private DataGridViewTextBoxColumn ColumnMajor;
+        private DataGridViewTextBoxColumn Column1;
+    }
+}
---
+++ CVSReader (ตัวอย่างนี้คือมีคนช่วยทำ ปกติผมทำไม่ได้ขนาดนี้ครับ)/CVSReader/Form1.cs
@@ -0,0 +1,162 @@
+﻿using ExcelDataReader;
+using System;
+using System.Data;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CVSReader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DGV.Rows.Clear();
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "CSV(*.csv) | *.csv" })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    //Excel Version xls อย่าลืมเปลี่ยน Filter นาาจ๊ะ
+                    //DGV.Rows.Clear();
+                    //if (ofd.FileName != "")
+                    //{
+                    //    try
+                    //    {
+                    //        FileStream stream = File.Open(ofd.FileName, FileMode.Open, FileAccess.Read);
+                    //        IExcelDataReader excelReader;
+
+                    //        if (Path.GetExtension(ofd.FileName).ToUpper() == ".XLS")
+                    //        {
+                    //            excelReader = ExcelReaderFactory.CreateBinaryReader(stream);
+                    //        }
+                    //        else
+                    //        {
+                    //            excelReader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+                    //        }
+                    //        DataSet result = excelReader.AsDataSet(new ExcelDataSetConfiguration()
+                    //        {
+                    //            ConfigureDataTable = (_) => new ExcelDataTableConfiguration()
+                    //            {
+                    //                UseHeaderRow = true
+                    //            }
+                    //        });
+                    //        DataSet ds = result;
+                    //        stream.Close();
+                    //        double GPA = 0.0;
+                    //        if (ds.Tables[0].Rows.Count != 0)
+                    //            for (int x = 0; x < ds.Tables[0].Rows.Count; x++)
+                    //            {
+                    //                DGV.Rows.Add(ds.Tables[0].Rows[x][0].ToString(), ds.Tables[0].Rows[x][1].ToString(), ds.Tables[0].Rows[x][2].ToString(), ds.Tables[0].Rows[x][3].ToString());
+                    //            }
+                    //        Cal();
+                    //    }
+                    //    catch
+                    //    {
+                    //    }
+                    //}
+
+                    //CSV Version csv
+                    string[] readAllLine = File.ReadAllLines(ofd.FileName);
+                    
+                    for (int i = 0; i < readAllLine.Length; i++)
+                    {
+                        string studentRAW = readAllLine[i];
+                        string[] studentSplied = studentRAW.Split(',');
+                        DGV.Rows.Add(studentSplied[0], studentSplied[1], studentSplied[2], studentSplied[3]);
+                    }
+                    Cal();
+                }
+            }
+        }
+        void Cal()
+        {
+            double GPA = 0;
+            double Max = 0;
+            double Min = 0;
+            Max = Convert.ToDouble(DGV.Rows[0].Cells[3].Value.ToString());
+            Min = Convert.ToDouble(DGV.Rows[0].Cells[3].Value.ToString());
+            for (int x = 0; x < DGV.Rows.Count; x++)
+            {
+                GPA += Convert.ToDouble(DGV.Rows[x].Cells[3].Value.ToString());
+                if (Max < Convert.ToDouble(DGV.Rows[x].Cells[3].Value.ToString()))
+                    Max = Convert.ToDouble(DGV.Rows[x].Cells[3].Value.ToString());
+
+                if (Min > Convert.ToDouble(DGV.Rows[x].Cells[3].Value.ToString()))
+                    Min = Convert.ToDouble(DGV.Rows[x].Cells[3].Value.ToString());
+            }
+            TBMax.Text = Max.ToString("N");
+            TBMin.Text = Min.ToString("N");
+            TBGpax.Text = (GPA / Convert.ToDouble(DGV.Rows.Count)).ToString("N");
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //string data = this.dataGridView1.Rows[0].Cells[0].Value.ToString();
+
+            string strData = null;
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "CSV (*.csv) | *.csv";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                if (saveFileDialog.FileName != null)
+                {
+                    for (int x = 0; x < DGV.Rows.Count; x++)
+                    {
+                        for (int y = 0; y < DGV.Columns.Count; y++)
+                        {
+                            strData +=  DGV.Rows[x].Cells[y].Value.ToString() + ",";
+                            if (y == DGV.Columns.Count - 1)
+                                strData += "\r\n";
+                        }
+                    }
+                    //save File
+                    File.WriteAllText(saveFileDialog.FileName, strData, Encoding.UTF8);
+                }
+
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(TBID.Text != "" && TBName.Text != "" && CBMajor.SelectedIndex != -1 && TBGPA.Text != "")
+            {
+                DGV.Rows.Add(TBID.Text, TBName.Text, CBMajor.SelectedItem, Convert.ToDouble(TBGPA.Text).ToString("N"));
+                Cal();
+                TBID.Text = "";
+                TBName.Text = "";
+                CBMajor.SelectedIndex = -1;
+                TBGPA.Text = "";
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+        }
+    }
+}
